e1d8f1b6283f4e187fde95df87be198cd540ec6d
sot <soul_of_thinking@163.com>
2023-05-10 22:57:39 +0800

    补充

--- utils.py
+++ utils.py
@@ -1,6 +1,19 @@
+import torch
+from matplotlib import pyplot as plt
+
+def plot_curve(data):
+    fig = plt.figure()
+    plt.plot(range(len(data)), data, color='blue')
+    plt.legend(['value'], loc='upper right')
+    plt.xlabel('step')
+    plt.ylabel('value')
+    plt.show()
+
+
 def abs(number):
     if not isinstance(number, (float, int)):
         return number
     if number < 0:
         number = number * -1
     return number
+
