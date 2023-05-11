f66e0672d5b5e5cb28a565c86a6bd24c192f4583
sot <soul_of_thinking@163.com>
2023-05-11 22:53:08 +0800

    plot_image

--- utils.py
+++ utils.py
@@ -9,6 +9,19 @@ def plot_curve(data):
     plt.ylabel('value')
     plt.show()
 
+def plot_image(img, label, name):
+    fig = plt.figure()
+    for i in range(6):
+        plt.subplot(2, 3, i + 1)
+        plt.tight_layout()
+        plt.imshow(img[i][0]*0.3081+0.1307, cmap='gray', interpolation='none')
+        plt.title("{}:{}".format(name, label[i].item()))
+        plt.xticks([])
+        plt.yticks([])
+    plt.show()
+
+
+
 
 def abs(number):
     if not isinstance(number, (float, int)):
